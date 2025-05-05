6a7fbfee7004edd77b2b457a3904ae433e29f61d
sample <sample@gmail.com>
2025-05-05 20:06:20 +0530

    shell script

--- mysql.sh
+++ mysql.sh
@@ -48,10 +48,12 @@ VALIDATION $? "starting mysql server"
 mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOG_FILE_NAME
 VALIDATION $? "setting password"
 
-mysql -h mysql.santhoshdatti.online -u root -pExpenseApp@1 -e 'show databases';
+mysql -h mysql.santhoshdatti.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then 
+    echo "mysql password is not setup" &>>$LOG_FILE_NAME
     mysql_secure_installation --set-root-pass ExpenseApp@1  &>>$LOG_FILE_NAME
+    VALIDATION $? "setting mysql password"
 else
     echo "mysql password is already setup"
 fi
